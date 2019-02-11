4c6461647b0b0e1918d035a219b3e428c47d5258
keeganosler <oslerkeegan@gmail.com>
2019-02-11 13:26:42 -0700

    image files and find next passover time

--- iss.py
+++ iss.py
@@ -38,7 +38,7 @@ print('Longitude: ', longitude)
 screen = turtle.Screen()
 screen.setup(720, 360)
 screen.setworldcoordinates(-180, -90, 180, 90)
-screen.bgpic('map.jpg')
+screen.bgpic('/images/map.gif')
 
 screen.register_shape('iss.png')
 iss = turtle.Turtle()
@@ -47,3 +47,28 @@ iss.setheading(90)
 
 iss.penup()
 iss.goto(longitude, latitude)
+
+
+
+## FIND WHEN THE ISS WILL NEXT BE OVER A CERTAIN LOCATION
+## to practice we will do this for Calgary, AB
+
+latitude = 51.048615
+longitude = -114.070847
+
+location = turtle.Turtle()
+location.penup
+location.color('yellow')
+location.goto(longitude, latitude)
+location.dot(5)
+location.hideturtle()
+
+url = 'http://api.open-notify.org/iss-pass.json'
+url = url + '?lat=' + str(latitude) + '&lon=' + str(longitude)
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+passover = result['response'][1]['risetime']
+
+style = ('Arial', 6, 'bold')
+location.write(time.ctime(passover), font=style)
